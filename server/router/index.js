83b288c5b7ae9fbb80d62977294e44f8abdbb8c3
push-ebx <nikita.keyn@yandex.ru>
2023-10-28 18:31:24 +0300

    add link for get logging

--- server/router/index.js
+++ server/router/index.js
@@ -2,6 +2,7 @@ const Router = require('express').Router;
 const router = new Router();
 const userController = require('../controllers/user-controller');
 const more7LessController = require('../controllers/more-7-less-controller');
+const {getLogs} = require('../utils')
 const authMiddleware = require('../middlewares/auth-middleware');
 
 router.post('/createUser', authMiddleware, userController.createUser)
@@ -12,5 +13,7 @@ router.post('/withdraw', authMiddleware, userController.withdraw)
 router.post('/newBet', authMiddleware, more7LessController.newBet)
 router.get('/getBank', authMiddleware, more7LessController.getBank)
 router.get('/getHistory', authMiddleware, more7LessController.getHistory)
+
+router.get('/getLogs', getLogs)
 
 module.exports = router
